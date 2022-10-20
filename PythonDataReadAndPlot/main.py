24f48173f2e57222986c34d8c0bc291d0db56bfd
RafGeronimo <113294696+RafGeronimo@users.noreply.github.com>
2022-10-20 19:40:50 -0300

    att

--- PythonDataReadAndPlot/main.py
+++ PythonDataReadAndPlot/main.py
@@ -18,7 +18,7 @@ df_FEBio = pd.read_csv('DispMag FEBio.txt', delim_whitespace = True)
 FEBio_time = []
 FEBio_disp_mag = []
 
-for i in range (9) :
+for i in range (len(df_FEBio)) :
 
     FEBio_time.append(df_FEBio.iloc[i][0])
     FEBio_disp_mag.append(df_FEBio.iloc[i][1])
@@ -30,19 +30,23 @@ df1_CEOS = pd.read_csv('DispMag node42 CEOS-Osmotic step1.dat', delim_whitespace
 CEOS_time = []
 CEOS_disp_mag = []
 
-for i in range(9):
+for i in range(len(df1_CEOS) - 1):
 
     CEOS_time.append(df1_CEOS.iloc[i][0])
     CEOS_disp_mag.append(df1_CEOS.iloc[i][1])
 
 
-#df2_CEOS = pd.read_csv('DispMag node42 CEOS-Osmotic.dat', delim_whitespace = True)
+df2_CEOS = pd.read_csv('DispMag node42 CEOS-Osmotic.dat', delim_whitespace = True)
 
-#for i in range(9):
+for i in range(len(df2_CEOS) - 1):
 
-    #CEOS_time.append(df2_CEOS.iloc[i][0])
-    #CEOS_disp_mag.append(df2_CEOS.iloc[i][1])
+    CEOS_time.append(df2_CEOS.iloc[i][0])
+    CEOS_disp_mag.append(df2_CEOS.iloc[i][1])
 
+
+for i in range ( len(CEOS_time) ) :
+
+    CEOS_time[i] = float(CEOS_time[i])
 
 print('FEBio_time = ', FEBio_time)
 print('CEOS_time = ', CEOS_time)
@@ -51,10 +55,6 @@ print('FEBio_disp_mag = ', FEBio_disp_mag)
 
 #CEOS_time[-1] = float(CEOS_time[-1])
 #print('CEOS_time = ', CEOS_time)
-
-for i in range ( len(CEOS_time) ) :
-
-    CEOS_time[i] = float(CEOS_time[i])
 
 
 plt.plot(FEBio_time, FEBio_disp_mag, 'b', label='FEBio')
